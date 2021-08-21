0349ebe3668f200faa0e3269a7f45c054708b3d8
aik <anthonykrueger0@gmail.com>
2021-08-20 19:37:25 -0500

    custom alerts added to login page

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,12 +2,18 @@ import {useState} from 'react'
 import axios from 'axios'
 import InputField from './InputField'
 import Button from './Button'
+import Alert from './Alert'
 
 export default function LoginForm({loginFunction}) {
     const [user, setField] = useState({"username": "", "password": ""})
+    const [alerts, setAlert] = useState({show: false, message:null})
 
     function handleChange(field, value) {
         setField({...user, [field]: value})
+    }
+
+    function hideAlert() {
+        setAlert({...alerts, show: false})
     }
 
     async function handleLogin(event) {
@@ -19,14 +25,15 @@ export default function LoginForm({loginFunction}) {
         .catch(function (error) {
             if(error.response) {
                 console.log(error.response)
-                alert(error.response.data.message)
+                setAlert({show: true, message: error.response.data.message})
             }
             else if(error.request) {
                 console.log(error.request);
-                alert(error.request)
+                setAlert({show: true, message: error.request})
             }
             else {
                 console.log("error:", error.message)
+                setAlert({show: true, message: error.message})
             }
         })
         if(response) {
@@ -41,7 +48,7 @@ export default function LoginForm({loginFunction}) {
     return (
         <div className="h-full">
             <form className="bg-orange-dark h-full content-center flex flex-wrap space-y-2 shadow-md rounded px-8 pt-6 pb-4 mb-2" onSubmit={(event) => handleLogin(event)}>
-                <h2 className="text-xl pb-5 text-white font-semibold">Login</h2>
+                <h2 className="text-xl pb-5 text-white underline font-semibold">Login</h2>
 
                 <InputField 
                 type="text" 
@@ -58,6 +65,9 @@ export default function LoginForm({loginFunction}) {
                 />
                 <div className="pt-5 mx-auto">
                     <Button type="submit" text="Login"/>
+                </div>
+                <div className="w-full">
+                    {alerts.show ? <Alert message={alerts.message} hideFunction={hideAlert}/> : null}
                 </div>
             </form>
         </div>
--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -2,12 +2,18 @@ import {useState} from 'react'
 import axios from 'axios'
 import InputField from './InputField'
 import Button from './Button'
+import Alert from './Alert'
 
 export default function SignUpForm({loginFunction}) {
     const [user, setField] = useState({"username": "", "password": "", "email": ""})
+    const [alerts, setAlert] = useState({show: false, message:null})
 
     function handleChange(field, value) {
         setField({...user, [field]: value})
+    }
+
+    function hideAlert() {
+        setAlert({...alerts, show: false})
     }
 
     async function handleSignUp(event) {
@@ -20,16 +26,16 @@ export default function SignUpForm({loginFunction}) {
         .catch(function (error) {
             if(error.response) {
                 if(error.response.data.code === 11000) {
-                    alert("Email or Username Not Available")
+                    setAlert({show: true, message: "Email or Username Not Available"})
                 }
                 else {
-                    alert(error.response.data.message)
+                    setAlert({show: true, message: error.response.data.message})
                 }
                 console.log(error.response.data)
             }
             else if(error.request) {
                 console.log(error.request);
-                alert(error.request)
+                setAlert({show: true, message: error.request})
             }
             else {
                 console.log("error:", error.message)
@@ -47,7 +53,7 @@ export default function SignUpForm({loginFunction}) {
     return (
         <div className="h-full">
             <form className="bg-orange-dark h-full content-center flex flex-wrap space-y-2 shadow-md rounded px-8 pt-6 pb-4 mb-2" onSubmit={(event) => handleSignUp(event)}>
-                <h2 className="text-xl pb-5 text-white font-semibold">Sign Up</h2>
+                <h2 className="text-xl pb-5 text-white underline font-semibold">Sign Up</h2>
                 <InputField type="text" placeholder="Email" value={user.email} onChange={(event) => handleChange("email", event.target.value)} />
                 <div className="grid grid-cols-2 w-full gap-2">
                     <InputField type="text" placeholder="Username" value={user.username} onChange={(event) => handleChange("username", event.target.value)} />
@@ -55,6 +61,9 @@ export default function SignUpForm({loginFunction}) {
                 </div>
                 <div className="pt-5 mx-auto">
                     <Button type="submit" text="Sign-Up"/>
+                </div>
+                <div className="w-full">
+                    {alerts.show ? <Alert message={alerts.message} hideFunction={hideAlert}/> : null}
                 </div>
             </form>
         </div>
